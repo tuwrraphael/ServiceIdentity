dc6063162089b66596b04444c77b373931bfc2fa
Raphael <raphael.hauk@racing.tuwien.ac.at>
2018-02-14 14:03:02 +0100

    initial config

--- ServiceIdentity/Startup.cs
+++ ServiceIdentity/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -11,32 +8,75 @@ using Microsoft.Extensions.DependencyInjection;
 using ServiceIdentity.Data;
 using ServiceIdentity.Models;
 using ServiceIdentity.Services;
+using System.IO;
+using IdentityServer4.Models;
+using System.Security.Cryptography.X509Certificates;
+using System.Reflection;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4;
+using IdentityServer4.EntityFramework.Mappers;
 
 namespace ServiceIdentity
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
             Configuration = configuration;
+            HostingEnvironment = environment;
+            environment.WebRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+
         }
 
+        
+
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment HostingEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = $"Data Source={HostingEnvironment.WebRootPath}\\App_Data\\users.db";
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlite(connectionString)
+            );
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
+
+            var cert = new X509Certificate2(Path.Combine(HostingEnvironment.WebRootPath, "App_Data", "cert.pfx"), "");
+
+            services.AddIdentityServer()
+                .AddSigningCredential(cert)
+                    .AddConfigurationStore(options =>
+                    {
+                        options.ConfigureDbContext = builder =>
+                            builder.UseSqlite(connectionString,
+                                sql => sql.MigrationsAssembly(migrationsAssembly));
+                    })
+                    // this adds the operational data from DB (codes, tokens, consents)
+                    .AddOperationalStore(options =>
+                    {
+                        options.ConfigureDbContext = builder =>
+                            builder.UseSqlite(connectionString,
+                                sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                        // this enables automatic token cleanup. this is optional.
+                        options.EnableTokenCleanup = true;
+                        options.TokenCleanupInterval = 30;
+                    })
+
+                .AddAspNetIdentity<ApplicationUser>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,10 +92,10 @@ namespace ServiceIdentity
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-
+            InitializeDatabase(app);
             app.UseStaticFiles();
 
-            app.UseAuthentication();
+            app.UseIdentityServer();
 
             app.UseMvc(routes =>
             {
